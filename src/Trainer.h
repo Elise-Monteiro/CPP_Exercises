a10dce445a88153aff6f0752e064de31b3bd0cc0
Elise <elise.ma.monteiro@gmail.com>
2023-02-07 15:58:13 +0100

    test 7

--- src/Trainer.h
+++ src/Trainer.h
@@ -1,6 +1,10 @@
 #pragma once
 #include <string>
+#include <array>
 #include "PC.h"
+#include "Pokeball.h"
+
+using Equipe = std::array<Pokeball, 6>;
 
 // A person that captures Pokemons and makes them fight.
 class Trainer
@@ -8,6 +12,7 @@ class Trainer
 private:
     const std::string _name;
     PC &_pc;
+    Equipe _pokeballs;
 
 public:
     Trainer(const std::string &name, PC &pc) : _name{name}, _pc{pc}
@@ -16,5 +21,9 @@ public:
     const std::string &name() const
     {
         return _name;
+    }
+    const Equipe &pokeballs() const
+    {
+        return _pokeballs;
     }
 };
