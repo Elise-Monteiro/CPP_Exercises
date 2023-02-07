3f5f608747481e887359b817ebe9a2173ff37723
Elise <elise.ma.monteiro@gmail.com>
2023-02-07 14:49:17 +0100

    test 1

--- src/Pokemon.h
+++ src/Pokemon.h
@@ -1,7 +1,44 @@
 #pragma once
 
 #include <string>
+#include <memory>
 
 // A creature that is cute and can fight other ones.
 class Pokemon
-{};
+{
+private:
+    std::string _name;
+    const int _id = 0;
+    static inline int next_id = 0;
+
+public:
+    Pokemon(const std::string &name)
+        : _name{name},
+          _id{next_id++}
+    {
+    }
+    Pokemon(const Pokemon &other)
+        : _name{other._name},
+          _id{next_id++}
+    {
+    }
+
+    const std::string &name() const
+    {
+        return _name;
+    }
+
+    int id() const
+    {
+        return _id;
+    }
+
+    Pokemon &operator=(const Pokemon &other)
+    {
+        if (this == &other)
+        {
+            _name = other._name;
+        }
+        return *this;
+    }
+};
