bffe82e0df106e4e74e3c44e668952b687d1510f
Elise <elise.ma.monteiro@gmail.com>
2023-02-07 15:28:48 +0100

    test 4

--- src/PC.h
+++ src/PC.h
@@ -1,6 +1,17 @@
 #pragma once
+#include "Pokemon.h"
+#include <vector>
 
 // A PC is the place where Pokemons get automagically sent when the trainer's pockets are full.
 // When a Pokemon is transferred to the PC, this one takes ownership.
 class PC
-{};
+{
+private:
+    std::vector<PokemonPtr> _pokemons;
+
+public:
+    std::vector<PokemonPtr> &pokemons()
+    {
+        return _pokemons;
+    }
+};
