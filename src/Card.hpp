05f5f5ea0439af582cf258f86156b780f8d463fa
Elise-Monteiro <elise.ma.monteiro@orange.fr>
2023-02-16 14:38:13 +0100

    test 1

--- src/Card.hpp
+++ src/Card.hpp
@@ -1,1 +1,20 @@
 #pragma once
+#include <string>
+//
+enum CardType {
+    Monster, Spell, Trap
+};
+
+inline std::string const to_string(const CardType& cardType) {
+    switch(cardType) {
+        case Monster :
+return "Monster";
+case Spell:
+return "Spell";
+case Trap:
+return "Trap";
+default:
+return "";
+
+    }
+}
