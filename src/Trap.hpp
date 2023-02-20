91235af1392278b7a9b2266d62309781147e2764
Elise <elise.ma.monteiro@gmail.com>
2023-02-20 19:29:41 +0100

    test 7

--- src/Trap.hpp
+++ src/Trap.hpp
@@ -1,1 +1,42 @@
 #pragma once
+#include <string>
+#include "Card.hpp"
+
+enum class TrapType
+{
+    Normal,
+    Continuous,
+    Counter
+};
+
+inline std::string const to_string(const TrapType trapType)
+{
+    switch (trapType)
+    {
+    case TrapType::Normal:
+        return "Normal";
+    case TrapType::Continuous:
+        return "Continuous";
+    case TrapType::Counter:
+        return "Counter";
+    default:
+        return "";
+    }
+}
+
+class Trap : public Card
+{
+public:
+    Trap(const std::string &id, const std::string &name, const TrapType &trapType)
+        : Card{id, CardType::Trap, name, ""},
+          _trapType{trapType}
+    {
+    }
+    TrapType get_trap_type() const
+    {
+        return _trapType;
+    }
+
+private:
+    const TrapType _trapType;
+};
