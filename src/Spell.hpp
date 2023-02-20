92555c2abd01d00d1b6268b477dda1d44bfeb8c2
Elise <elise.ma.monteiro@gmail.com>
2023-02-20 19:18:00 +0100

    test 6

--- src/Spell.hpp
+++ src/Spell.hpp
@@ -1,1 +1,48 @@
 #pragma once
+#include <string>
+#include "Card.hpp"
+
+enum class SpellType
+{
+    Normal,
+    Equip,
+    Continuous,
+    QuickPlay,
+    Field
+};
+
+inline std::string const to_string(const SpellType spellType)
+{
+    switch (spellType)
+    {
+    case SpellType::Normal:
+        return "Normal";
+    case SpellType::Equip:
+        return "Equip";
+    case SpellType::Continuous:
+        return "Continuous";
+    case SpellType::QuickPlay:
+        return "Quick-Play";
+    case SpellType::Field:
+        return "Field";
+    default:
+        return "";
+    }
+}
+
+class Spell : public Card
+{
+public:
+    Spell(const std::string &id, const std::string &name, const SpellType &spellType)
+        : Card{id, CardType::Spell, name, ""},
+          _spellType{spellType}
+    {
+    }
+    SpellType get_spell_type() const
+    {
+        return _spellType;
+    }
+
+private:
+    const SpellType _spellType;
+};
