1af74c8a8f45cab977f641b730c85c78f5665335
Elise-Monteiro <elise.ma.monteiro@orange.fr>
2023-02-21 15:21:46 +0100

    test 3

--- src/StringLeaf.hpp
+++ src/StringLeaf.hpp
@@ -1,1 +1,16 @@
 #pragma once
+#include "Leaf.hpp"
+#include <string>
+
+class StringLeaf : public Leaf
+{
+public:
+    StringLeaf(const std::string& string)
+        : _string { string }
+    {_type = NodeKind::STRING;}
+    std::string print() const override { return "\"" + _string + "\""; }
+    const std::string & data() const { return _string; }
+
+private:
+    const std::string _string;
+};
