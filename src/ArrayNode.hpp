d74540067d43a3464cbe12fd1368326ab805f128
Elise-Monteiro <elise.ma.monteiro@orange.fr>
2023-02-21 15:31:05 +0100

    test 4

--- src/ArrayNode.hpp
+++ src/ArrayNode.hpp
@@ -1,1 +1,19 @@
 #pragma once
+#include "Node.hpp"
+#include <vector>
+#include <string>
+
+class ArrayNode : public Node
+{
+    public:
+        ArrayNode() {
+            _type = NodeKind::ARRAY;
+        }
+
+        virtual std::string print() const override{//non complete
+            std::string str = "[";
+            return str + "]";
+        }
+    private:
+        std::vector<Node> _array;
+};
