f5e985552d78066b8eeb300bf2f0e4b42cc01d8f
Elise-Monteiro <elise.ma.monteiro@orange.fr>
2023-03-14 15:26:09 +0100

    test 1

--- src/Kitchen.hpp
+++ src/Kitchen.hpp
@@ -9,16 +9,22 @@
 class Kitchen
 {
 public:
-    /*const Unit& register_unit(Unit unit)
+    const Unit& register_unit(Unit unit)
     {
-        auto [it, emplaced] = units.emplace(std::move(unit));
+        auto [it, emplaced] = _units.emplace(std::move(unit));
         return *it;
-    }*/
-    const Unit& register_unit(const Unit& unit)
+    }
+    const Unit* find_unit(const std::string& name) const
+{
+    const auto it = _units.find(Unit { name });
+    return it != _units.end() ? &(*it) : nullptr;
+}
+    /*const Unit& register_unit(const Unit& unit)
     {
         _units.emplace_back(std::move(unit));
         return *(_units.rbegin());
-    }
+    }*/
+    /*
     const Unit* find_unit(const std::string& name) const
     {
         for (const auto& e : _units)
@@ -29,13 +35,24 @@ public:
             }
         }
         return nullptr;
-    }
-    /*
-    bool compare_unit(const Unit& unit1, const Unit& unit2)const {
-        return unit1.name.compare(unit2.name);
     }*/
 
 private:
-    /*std::set<Unit, compare_unit> _units;*/
-    std::vector<Unit> _units;
+ struct ElementNameComparer
+    {
+        bool compare_insensitive(const std::string& s1, const std::string& s2) const
+        {
+            return std::lexicographical_compare(s1.begin(), s1.end(), s2.begin(), s2.end(),
+                                                [](char c1, char c2) { return tolower(c1) < tolower(c2); });
+        }
+        bool operator()(const Unit& u1, const Unit& u2) const { return u1.name < u2.name; }
+        bool operator()(const Unit& unit, const std::string& name) const { return unit.name < name; }
+        bool operator()(const std::string& name, const Unit& unit) const { return name < unit.name; }
+
+    };
+    /*bool compare_unit(const Unit& unit1, const Unit& unit2)const {
+        return unit1.name.compare(unit2.name);
+    }*/
+    std::set<Unit, ElementNameComparer> _units;
+    /*std::vector<Unit> _units;*/
 };
