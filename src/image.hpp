3e9241cad6e9e8138b5d5909c886c39c2efaf7da
Elise <elise.ma.monteiro@gmail.com>
2023-03-28 19:11:45 +0200

    test 101

--- src/image.hpp
+++ src/image.hpp
@@ -1,13 +1,37 @@
 #pragma once
 
+#include "../lib/lib.hpp"
 #include "pixels.hpp"
 
-#include "../lib/lib.hpp"
-
-#include <stdint.h>
+#include <array>
 #include <cstddef>
 #include <functional>
-#include <string>
-#include <array>
 #include <iostream>
+#include <stdint.h>
+#include <string>
+// i, j (colonne, ligne)
+template <typename P, size_t W, size_t H> //
+class Image
+{
+private:
+    std::array<std::array<P, W>, H> _image;
 
+public:
+    Image() = default;
+    Image(const P& pix)
+    {
+        for (size_t i = 0; i < W; i++)
+        { // colonne
+            for (size_t j = 0; j < H; j++)
+            { // ligne
+                _image[i][j] = pix;
+            }
+        }
+    }
+    /*
+        P& operator(const size_t i, const size_t j)
+        { // colonne ligne
+            return _image[i][j];
+        }
+    const P& operator(const size_t i, const size_t j) const { return _image[i][j]; }*/
+};
