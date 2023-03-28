0d8887844c6259a030a126c44bcc63232b634aa2
Elise <elise.ma.monteiro@gmail.com>
2023-03-28 20:08:50 +0200

    test 103

--- src/image.hpp
+++ src/image.hpp
@@ -33,4 +33,31 @@ public:
     {
         return _image[j][i];
     } // pour l'opération (x, y)
+
+    // function = lambda donc fonction
+    /*
+    Image<P, W, H>(const std::function<P(const size_t i, const size_t j)>& functor)
+    {
+        for (size_t j = 0; j < H; j++)
+        { // ligne
+            for (size_t i = 0; i < W; i++)
+            { // colonne
+                (*this)(i, j) = functor(i, j);
+            }
+        }
+    }*/
 };
+
+template <typename P, size_t W, size_t H>
+Image<P, W, H> operator+(const Image<P, W, H>& i1, const Image<P, W, H>& i2)
+{
+    Image<P, W, H> image = {};
+    for (size_t j = 0; j < H; j++)
+    { // ligne
+        for (size_t i = 0; i < W; i++)
+        { // colonne
+            image(i, j) = i1(i, j) + i2(i, j);
+        }
+    }
+    return image;
+}
