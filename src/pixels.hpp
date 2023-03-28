60f960b23ed35b984c2cafe523aa73c120cfb781
Elise <elise.ma.monteiro@gmail.com>
2023-03-28 21:07:38 +0200

    test 301

--- src/pixels.hpp
+++ src/pixels.hpp
@@ -4,3 +4,37 @@
 
 #include <stdint.h>
 
+struct RGBA
+{
+public:
+    unsigned int r;
+    unsigned int g;
+    unsigned int b;
+    unsigned int a;
+    RGBA(unsigned int _r, unsigned int _g, unsigned int _b, unsigned int _a)
+        : r { _r }
+        , g { _g }
+        , b { _b }
+        , a { _a }
+    {}
+};
+
+struct RGB
+{
+    unsigned int r;
+    unsigned int g;
+    unsigned int b;
+    RGB(unsigned int _r, unsigned int _g, unsigned int _b)
+        : r { _r }
+        , g { _g }
+        , b { _b }
+    {}
+};
+
+struct Luma
+{
+    unsigned int gray;
+    Luma(unsigned int _gray)
+        : gray { _gray }
+    {}
+};
