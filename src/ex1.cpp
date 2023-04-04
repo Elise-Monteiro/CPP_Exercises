0a407a0aaed855577e1b30123d0d0f5e692a78a9
Elise-Monteiro <elise.ma.monteiro@orange.fr>
2023-04-04 14:48:00 +0200

    test 12

--- src/ex1.cpp
+++ src/ex1.cpp
@@ -1,1 +1,22 @@
 #include "ex1.hpp"
+#include "../lib/concatenate.hpp"
+
+void pairwise_concatenate(std::list<std::list<int>>& list1, const std::list<std::list<int>>& list2) {//utilisé les iterator
+    auto itL1 = list1.begin();
+    auto itL2 = list2.begin();
+    for(; itL1 != list1.end() && itL2 != list2.end(); itL1++) {
+        concatenate(*itL1, *itL2);//on a le droit d'utiliser concatenate pas obliger d'avoir une seconde boucle
+        itL2++;
+    }
+    return;
+}
+/*
+void pairwise_concatenate(std::list<std::list<int>>& list1, const std::list<std::list<int>>&& list2) {//utilisé les iterator
+    auto itL1 = list1.begin();
+    auto itL2 = list2.begin();
+    for(; itL1 != list1.end() && itL2 != list2.end(); itL1++) {
+        concatenate(*itL1, *itL2);//on a le droit d'utiliser concatenate pas obliger d'avoir une seconde boucle
+        itL2++;
+    }
+    return;
+}*/
