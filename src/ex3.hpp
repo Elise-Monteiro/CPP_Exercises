7714f584a4a4b4355e4671b19eb238689e3ee9d6
Elise-Monteiro <elise.ma.monteiro@orange.fr>
2023-04-04 16:02:37 +0200

    test 32 en cours

--- src/ex3.hpp
+++ src/ex3.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 template <int num>
-constexpr int fibonacci()//faut le constexpr sinon on peut pas faire std::array<int, fibonacci<1>()>
+constexpr int fibonacci() // faut le constexpr sinon on peut pas faire std::array<int, fibonacci<1>()>
 {
     if constexpr (num < 2) // pour evité que le compilateur le genére pour tous les nombres négatifs
     {
@@ -11,4 +11,10 @@ constexpr int fibonacci()//faut le constexpr sinon on peut pas faire std::array<
     { // faut mettre forcement le else
         return fibonacci<num - 1>() + fibonacci<num - 2>();
     }
+}
+
+template <typename T1, typename T2>
+constexpr bool content_of_same_type()
+{
+    return T1 == T2;
 }
