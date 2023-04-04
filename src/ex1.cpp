6f3f2cdf75cbf9c7c62177df56521c336e5caaf7
Elise-Monteiro <elise.ma.monteiro@orange.fr>
2023-04-04 14:54:13 +0200

    test 13

--- src/ex1.cpp
+++ src/ex1.cpp
@@ -10,13 +10,13 @@ void pairwise_concatenate(std::list<std::list<int>>& list1, const std::list<std:
     }
     return;
 }
-/*
-void pairwise_concatenate(std::list<std::list<int>>& list1, const std::list<std::list<int>>&& list2) {//utilisé les iterator
+
+void pairwise_concatenate(std::list<std::list<int>>& list1, std::list<std::list<int>>&& list2) {//pas oublié d'enlevé le const puisqu'on fait un move
     auto itL1 = list1.begin();
     auto itL2 = list2.begin();
     for(; itL1 != list1.end() && itL2 != list2.end(); itL1++) {
-        concatenate(*itL1, *itL2);//on a le droit d'utiliser concatenate pas obliger d'avoir une seconde boucle
+        concatenate(*itL1, std::move(*itL2));//std::move pour pas de copie
         itL2++;
     }
     return;
-}*/
+}
--- src/ex1.hpp
+++ src/ex1.hpp
@@ -3,4 +3,4 @@
 #include <list>
 
 void pairwise_concatenate(std::list<std::list<int>>& list1, const std::list<std::list<int>>& list2);
-//void pairwise_concatenate(std::list<std::list<int>>& list1, const std::list<std::list<int>>&& list2);
+void pairwise_concatenate(std::list<std::list<int>>& list1, std::list<std::list<int>>&& list2);
